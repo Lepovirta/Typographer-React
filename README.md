23e74f31104d88ec1f48590bfa38d12d05c07a18
Esa Juhana Lahikainen <esa.lahikainen@solteq.com>
2017-01-10 17:41:46 +0200

    Add example

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
+# Typographer 2.0 (React.js + Google Fonts API port)
+
+![image of the app](./examples/screenshot.png)
+
 This project was bootstrapped with [Create React App](https://github.com/facebookincubator/create-react-app).
 
 Below you will find some information on how to perform common tasks.<br>
--- src/App.js
+++ src/App.js
@@ -20,16 +20,32 @@ class App extends Component {
     super(props);
     this.state = {
       typefaces: [],
-      typeface: ''
+      typeface: '',
+      firstLocked: false,
+      secondLocked: false
     };
     this.generateNew = this.generateNew.bind(this);
+    this.toggleLock = this.toggleLock.bind(this);
   }
 
   generateNew() {
     this.setState(prevState => ({
       typeface: newTypeface(prevState.typefaces)
     }));
-  }  
+  }
+
+  toggleLock(fontNumber) {
+    if(fontNumber === 1) {
+      this.setState(prevState => ({
+        firstLocked: !prevState.firstLocked
+      }));
+    }
+   if(fontNumber === 2) {
+      this.setState(prevState => ({
+        secondLocked: !prevState.secondLocked
+      }));
+    }    
+  }
 
   componentDidMount() {
     this.serverRequest = $.get(url, function (result) {
@@ -53,14 +69,24 @@ class App extends Component {
     return (
       <div className="App">
         <h1>The Typographer 2.0</h1>
-        <button onClick={this.generateNew}>New Combination (enter)</button>
-        {this.state.typeface && <Typeface typeface={this.state.typeface} /> }
+        <h2>Generate Awesome Typeface Combinations with Google Fonts API, Modular Scale and Randomization.</h2>
+        <aside className="functions">
+          <button onClick={this.generateNew}>New Combination (enter)</button>
+          <button className={this.state.firstLocked ? 'locked' : ''} onClick={() => this.toggleLock(1)}>Lock #1</button>
+          <button className={this.state.secondLocked ? 'locked' : ''} onClick={() => this.toggleLock(2)}>Lock #2</button>
+        </aside>
+        {this.state.typeface && 
+          <Typeface 
+            firstLocked={this.state.firstLocked} 
+            secondLocked={this.state.secondLocked} 
+            typeface={this.state.typeface} />
+        }
       </div>
     );
   }
 }
 
-const Typeface = ({typeface}) => {
+const Typeface = ({firstLocked, secondLocked, typeface}) => {
 
   $.get('https://fonts.googleapis.com/css?family='+typeface.family, function (result) {
     var newStyle = document.createElement('style');
@@ -69,19 +95,19 @@ const Typeface = ({typeface}) => {
   });  
   
   return (
-    <section style={{ fontFamily: typeface.family }} className="Typeface">
+    <section className="Typeface">
       <header>
         <h2><a title="View Specimen" href={'https://fonts.google.com/specimen/'+typeface.family.split(' ').join('+') }>{typeface.family} <span>({typeface.category})</span></a></h2>
         <h3><a title="Copy @font-face" href={'https://fonts.googleapis.com/css?family='+typeface.family.split(' ').join('+') }>@font-face</a></h3>
         <h3>{"<link rel='stylesheet' type='text/css' href='https://fonts.googleapis.com/css?family="+typeface.family.split(' ').join('+')+">"}</h3>
       </header>
-      <section>
-        <Headings />
+      <section style={{ fontFamily: typeface.family }}>
+        <Headings firstLocked={firstLocked} />
         <p>Google Fonts makes it quick and easy for everyone to use web fonts, including professional designers and developers. We believe that everyone should be able to bring quality typography to their web pages and applications.</p>
         <p>Our goal is to create a directory of web fonts for the world to use. Our API service makes it easy to add Google Fonts to a website in seconds. The service runs on Google's servers which are fast, reliable and tested. Google provides this service free of charge.</p>
         <p>All of the fonts are Open Source. This means that you are free to share your favorites with friends and colleagues. You can even customize them for your own use, or collaborate with the original designer to improve them. And you can use them in every way you want, privately or commercially — in print, on your computer, or in your websites.</p>
-        <Links files={typeface.files} />
       </section>
+      <Links files={typeface.files} />
     </section>
   );
 }
@@ -100,8 +126,7 @@ const Headings = () => {
       <p key={i} style={{ fontSize: scale[i]+'px' }}>Lorem Ipsum Dipsum Pupsum</p>
     );
   }
-  headingExamples.reverse();  
-  
+  headingExamples.reverse();
   return (
     <section className="Headings">
       <div>
